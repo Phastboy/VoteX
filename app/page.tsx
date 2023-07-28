bf25dda79de38412dd2fcd5ada46dd704922febe
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-07-28 13:02:00 +0100

    components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
+'use client'
 import Image from 'next/image';
 import Link from 'next/link';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+
+
+
 
 export default function Home() {
   return (
 	<>
 		<h1>Home page</h1>
+		
 	</>
   );
 }
