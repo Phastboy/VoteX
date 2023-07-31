be04f09968986f14ab5858f52f2686d046d4fce6
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-07-31 04:16:31 +0100

    sign up

---
+++ app/dashboard copy/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Welcome() {
+  return (
+	<main>
+		<h1>welcome to votex</h1>
+	</main>
+  )
+}
---
+++ app/dashboard copy/settings/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Welcome() {
+  return (
+	<main>
+		<h1>settings</h1>
+	</main>
+  )
+}
