36553862979538add4aa8eb018347e0d0a6b5f41
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-08-02 04:14:26 +0100

    settings

---
+++ app/dashboard/settings/account-settings/page.tsx
@@ -0,0 +1,13 @@
+export default function Profile(){
+	return (
+		<>
+			<h1>Account settings</h1>
+			<div>
+				<h6>Profile</h6>
+				<h6>Appearance</h6>
+				<h6>Security</h6>
+				<h6>Deactivate account</h6>
+			</div>
+		</>
+	);
+}
