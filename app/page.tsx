1e60900b44bf404529b55d50d14c1c018e9bd030
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-08-07 14:37:43 +0100

    home

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 	<main className="">
 		<div className="bg-[url('/bg-top.png')] bg-auto h-screen bg-gray-900">
 		<header className="flex space-x-20 p-5">
-          <div >
+          <div className='space-y-4'>
               <Link href="/" className="">
                 <Image src="/logo.svg"
                 alt="VoteX logo"
@@ -26,8 +26,7 @@ export default function Home() {
                 />
               </Link>
             </div>
-            <Menu/>
-          {/*<nav className="float-right space-x-4 p-10"> 
+          <nav className="float-right space-x-4 p-10"> 
             {[
               ['Pricing', '/pricing'],
               ['Reviews', '/reviews'],
@@ -35,32 +34,33 @@ export default function Home() {
               ['Login', '/login'],
               ['Sign Up', '/sign-up']
             ].map(([title, url]) => (
-            <Link href={url} className="">{title}</Link>
+            <Link href={url} className="rounded-lg px-3 py-2 text-white font-medium hover:bg-slate-100 hover:text-slate-900">{title}</Link>
             ))}
-          </nav>*/}
+          </nav>
         </header>
 			<div className="  text-white">
 				<div className="flex py-5">
-					<div className="flex-col">
+					<div className="flex-col p-10">
 						<h3>
 							<span className="font-semibold">Vote anytime, anywhere with </span>
 							<Image
 							src="/logo.png"
 							alt="votex logo"
-							width={500}
-							height={500}
-							style={{width: 'auto', height: 'auto'}}
+							width={100}
+							height={144}
 							/>
 							: YOUR SECURE ONLINE VOTING APP
 						</h3>
 						<p className='pt-10'>
 						Cast your vote anytime, anywhere with confidence. Simple, secure, and ready to empower your voice. Register now and make a difference!
 						</p>
+						<button className="bg-gray-100 text-black font-bold rounded py-2 px-4 mt-5">Get started</button>
 					</div>
 					<Image 
 					src="/cast_vote.png"
-					width={100}
-					height={100}
+					width={300}
+					height={300}
+					style={{width: 'auto', height: 'auto'}}
 					alt="votex logo"
 					className="float-right"
 					/>
@@ -169,7 +169,7 @@ export default function Home() {
 				/>
 				<h3>Empower Your Voice: Introducing VoteX Online Voting App</h3>
 				<p>Fostering leadership through every vote</p>
-				<div className="flex space-x-10 justify-center">
+				<div className="flex space-x-10 justify-center p-10">
 					<button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
 					Login
 					</button>
