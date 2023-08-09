e576114be0db1ee44405eeca1360de44d8562967
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-08-09 21:06:20 +0100

    dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,13 @@ import Image from 'next/image'
 export default function Welcome() {
   return (
 	<main>
-		<h1>welcome to votex</h1>
+		<div className="text-center py-10">
+			<h1>welcome to votex</h1>
+			<div className="flex flex-col md:flex-row p-10 justify-center items-center space-y-5 md:space-x-20 md:space-y-0">
+				<button className="btn btn-green">Create election</button>
+				<button className="btn btn-green">Create poll</button>
+			</div>
+		</div>
 	</main>
   )
 }
