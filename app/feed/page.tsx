af546d8ffd9b5a3d189bf6eb7af8d5c77b3c5751
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-08-09 11:50:00 +0100

    feed

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -6,12 +6,12 @@ export default function Home(){
 		<>
 			<h3 className="p-3">Good morning username</h3>
 			<h3 className="px-3">recent poll</h3>
-			<div className="flex flex-col items-center pb-10">
+			<div className="max-w-sm p-6 m-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
 				<h5 className="pt-5 mb-1 text-xl font-medium text-gray-900 dark:text-white">Are you a member?</h5>
 				<div className="flex mt-4 space-x-3 md:mt-6">
 					<Link href="/result" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Yes</Link>
 					<Link href="/result" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">No</Link>
-        </div>
+        		</div>
 			</div>
 			<h3>recent election</h3>
 			<h3>or maybe look around</h3>
