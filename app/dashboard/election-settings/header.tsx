1feaa62263dde5662b8d029fb60f21a4f4cc1d67
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-08-13 00:07:13 +0100

    components

--- app/dashboard/election-settings/header.tsx
+++ app/dashboard/election-settings/header.tsx
@@ -1,33 +1,45 @@
-import * as React from "react"
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
+interface SelectionProps {
+    setSelectedSetting: React.Dispatch<React.SetStateAction<string>>;
+}
 
-export function Selection() {
-	const [ selectedItem, setSelectedItem ]=useState('');
-  return (
-    <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="General" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectItem value="Dates" onClick={()=>setSelectedItem('Dates')}>Dates</SelectItem>
-          <SelectItem value="Voters" onClick={()=>setSelectedItem('Voters')}>Voters</SelectItem>
-          <SelectItem value="Messages" onClick={()=>setSelectedItem('Messages')}>Meessages</SelectItem>
-          <SelectItem value="Email" onClick={()=>setSelectedItem('Email')}>Email</SelectItem>
-          <SelectItem value="Result" onClick={()=>setSelectedItem('Result')}>Result</SelectItem>
-          <SelectItem value="Duplicate" onClick={()=>setSelectedItem('Duplicate')}>Duplicate</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-  )
+export function Selection({ setSelectedSetting }: SelectionProps) {
+    const [selectedItem, setSelectedItem] = useState('');
+
+    const handleSelect = (value: string) => {
+        setSelectedItem(value);
+        setSelectedSetting(value);
+    };
+
+    return (
+        <Select>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="General" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectItem value="Dates" onClick={() => handleSelect('Dates')}>
+                        Dates
+                    </SelectItem>
+                    <SelectItem value="Voters" onClick={() => handleSelect('Voters')}>
+                        Voters
+                    </SelectItem>
+                    <SelectItem value="Messages" onClick={() => handleSelect('Messages')}>
+                        Messages
+                    </SelectItem>
+                    <SelectItem value="Email" onClick={() => handleSelect('Email')}>
+                        Email
+                    </SelectItem>
+                    <SelectItem value="Result" onClick={() => handleSelect('Result')}>
+                        Result
+                    </SelectItem>
+                    <SelectItem value="Duplicate" onClick={() => handleSelect('Duplicate')}>
+                        Duplicate
+                    </SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+    );
 }
--- app/dashboard/election-settings/page.tsx
+++ app/dashboard/election-settings/page.tsx
@@ -1,35 +1,43 @@
 'use client'
-import { Label } from "@/components/ui/label";
+import * as React from "react";
+import { useState } from "react";
 import { Selection } from "./header";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import {Nav} from '../overview/nav';
-import { GeneralElectionSettings,
-    ElectionDatesSettings, 
-    ElectionEmailSettings, 
-    ElectionMessagesSettings, 
-    ElectionVotersSettings, 
-    ElectionResultSettings, 
-    ElectionDuplicationSettings } from "./election-settings";
-
+import { GeneralElectionSettings, ElectionDatesSettings, ElectionEmailSettings, ElectionMessagesSettings, ElectionVotersSettings, ElectionResultSettings, ElectionDuplicationSettings } from "./election-settings";
+import { Nav } from '../overview/nav';
 
 export default function ElectionSettings() {
+    const [selectedSetting, setSelectedSetting] = useState('General');
+
+    const renderSelectedSetting = () => {
+        switch (selectedSetting) {
+            case 'Dates':
+                return <ElectionDatesSettings />;
+            case 'Voters':
+                return <ElectionVotersSettings />;
+            case 'Messages':
+                return <ElectionMessagesSettings />;
+            case 'Email':
+                return <ElectionEmailSettings />;
+            case 'Result':
+                return <ElectionResultSettings />;
+            case 'Duplicate':
+                return <ElectionDuplicationSettings />;
+            default:
+                return <GeneralElectionSettings />;
+        }
+    };
+
     return (
         <>
-			<Nav/>
+            <Nav />
             <div className="p-4 space-y-6">
                 <div>
-                    <Selection/>
+                    <Selection setSelectedSetting={setSelectedSetting} />
                 </div>
                 <div>
-                    <h1>the specific setting</h1>
+                    <h1>{`The ${selectedSetting} Setting`}</h1>
                 </div>
-                <div>
-                    <GeneralElectionSettings/>
-                </div>
-                <div>
-                    <ElectionResultSettings/>
-                </div>
+                {renderSelectedSetting()}
             </div>
         </>
     );
