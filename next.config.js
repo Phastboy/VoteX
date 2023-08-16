dd347efc09a38bcdc186b993cf8056131ce6a319
Tmahn001 <toluiwalewa@gmail.com>
2023-08-16 20:35:16 +0100

    auth-fixed

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@
                 source: "/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://127.0.0.1:8000/" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Origin", value: "http://localhost:8000/" }, // replace this your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
