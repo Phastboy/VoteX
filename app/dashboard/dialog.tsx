a70f6f7f60c04410856c9a7c0e657e306286cac6
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-08-18 09:54:57 +0100

    login navigation

--- app/dashboard/dialog.tsx
+++ app/dashboard/dialog.tsx
@@ -1,7 +1,5 @@
-import React, {useState} from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 import { DatePickerWithPresets } from './date';
-import { Visibility } from './visibility';
-
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -15,7 +13,51 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
+interface FormData {
+  title: string;
+  start: string;
+  stop: string;
+  visibility: string;
+}
+
 function Election() {
+  const [formData, setFormData] = useState<FormData>({
+    title: '',
+    start: '',
+    stop: '',
+    visibility: 'Private',
+  });
+
+  const handleCreateElection = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('https://votex-backend.eastasia.cloudapp.azure.com/elections/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        alert('Election created successfully');
+      } else {
+        alert('Election creation failed');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -28,39 +70,66 @@ function Election() {
             Create your election the way you want.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
+        <form className="grid gap-4 py-4" onSubmit={handleCreateElection}>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="electionTitle" className="text-right">
               Title
             </Label>
-            <Input id="electionTitle" placeholder="SUG election" className="col-span-3" />
+            <Input
+              id="electionTitle"
+              name="title"
+              value={formData.title}
+              onChange={handleChange}
+              placeholder="SUG election"
+              className="col-span-3"
+            />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="electionStart" className="text-right">
               Starting
             </Label>
-            <DatePickerWithPresets/>
+            <DatePickerWithPresets
+              id="electionStart"
+              name="start"
+              value={formData.start}
+              onChange={handleChange}
+            />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="electionEnd" className="text-right">
               Ending
             </Label>
-            <DatePickerWithPresets/>
+            <DatePickerWithPresets
+              id="electionEnd"
+              name="stop"
+              value={formData.stop}
+              onChange={handleChange}
+            />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="electionVisibility" className="text-right">
               Visibility
             </Label>
-            <Visibility/>
+            <select
+              id="electionVisibility"
+              name="visibility"
+              value={formData.visibility}
+              onChange={handleChange}
+              className="col-span-3"
+            >
+              <option value="Private">Private</option>
+              <option value="Public">Public</option>
+            </select>
           </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit" variant="continue">Continue</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button type="submit" variant="continue">Continue</Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   )
 }
+
 
 function Poll() {
   const [options, setOptions] = useState([{ id: 1, value: "" }]);
@@ -149,7 +218,6 @@ function Poll() {
             <Label htmlFor="visibility" className="text-right">
               Visibility
             </Label>
-            <Visibility/>
           </div>
         </div>
         <DialogFooter>
