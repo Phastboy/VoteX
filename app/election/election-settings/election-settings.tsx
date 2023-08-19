42a914bd6ae8d0a6b4b40e6c7504a8965a26ea22
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2023-08-19 14:59:34 +0100

    created election successfully

--- app/election/election-settings/election-settings.tsx
+++ app/election/election-settings/election-settings.tsx
@@ -202,30 +202,35 @@
 //         </>
 //     );
 // }
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import React, { useEffect, useState } from 'react';
 
 export default function ElectionSettings() {
-  const electionId = localStorage.getItem('electionId');
   const accessToken = localStorage.getItem('accessToken');
-  const [settingsData, setSettingsData] = useState(null);
+  const electionId = localStorage.getItem('electionId');
+
+  const [electionSettings, setElectionSettings] = useState(null);
 
   const fetchSettings = async () => {
     try {
       const headers = {
         'Authorization': `Bearer ${accessToken}`,
-        'ElectionId': `${electionId}`,
         'Content-type': 'application/json'
       };
 
-      const response = await fetch(`https://votex-backend.eastasia.cloudapp.azure.com/elections/${electionId}/settings?category=Results`, {
+      const response = await fetch(`https://votex-backend.eastasia.cloudapp.azure.com/elections/${electionId}/settings`, {
         method: 'GET',
         headers: headers
       });
 
       if (response.ok) {
         const data = await response.json();
-        setSettingsData(data);
+        console.log(data);
+        alert('Settings fetched successfully');
+        setElectionSettings(data.data);
       } else {
+        alert('Error fetching settings');
         console.log('Error fetching settings');
       }
     } catch (error) {
@@ -235,15 +240,26 @@ export default function ElectionSettings() {
 
   useEffect(() => {
     fetchSettings();
-  }, []);
+  }, [electionId]);
 
   return (
     <div>
       <h1>Election Settings</h1>
-      {settingsData ? (
+      {electionSettings ? (
         <div>
-          <h2>Results Category Settings</h2>
-          <p>Setting 1: {settingsData}</p>
+          {electionSettings.map((settings) => (
+            <div key={settings.id} className='bg-white border border-gray-200 rounded-lg p-10 m-10'>
+              <Label>{settings.title}</Label>
+              {settings.setting_type === 'BOOLEAN' ? (
+              <label className="relative inline-flex items-center mb-5 cursor-pointer">
+                <Input type="checkbox" value="" className="sr-only peer" />
+                <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-700 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
+              </label>
+              ) : (
+                <p>{settings.value}</p>
+              )}
+            </div>
+          ))}
         </div>
       ) : (
         <p>Loading settings...</p>
